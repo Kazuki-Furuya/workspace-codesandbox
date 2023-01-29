7ccce8552a85c86e5ceb5e271df49fdd583674f2
kazuki-furuya <furuyakazuki@gmail.com>
2023-01-29 17:03:23 +0900

    4-24　スプレッド構文

--- src/index.js
+++ src/index.js
@@ -106,6 +106,37 @@
 // console.log(message4);
 
 // 4-23　デフォルト値
-const sayHello = (name = "ゲスト") => console.log(`こんにちは、${name}さん!`);
-sayHello("caz");
-sayHello();
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは、${name}さん!`);
+// sayHello("caz");
+// sayHello();
+
+// 4-24　スプレッド構文
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+// const arr6 = arr4;
+console.log(arr6);
+arr6[1] = 50;
+console.log(arr4);
+console.log(arr6);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
