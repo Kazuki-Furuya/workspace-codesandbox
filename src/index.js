8a7e431926adc12cf224ec242a90c201fc2f2143
kazuki-furuya <furuyakazuki@gmail.com>
2023-01-29 18:18:18 +0900

    4-25　mapやfilterを使った配列の処理

--- src/index.js
+++ src/index.js
@@ -120,23 +120,50 @@
 // sumFunc(arr1[0], arr1[1]);
 // sumFunc(...arr1);
 
-// まとめる
-const arr2 = [1, 2, 3, 4, 5];
-const [num1, num2, ...arr3] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(arr3);
+// // まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
 
-// 配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
+// // 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
 
-const arr6 = [...arr4];
-// const arr6 = arr4;
-console.log(arr6);
-arr6[1] = 50;
-console.log(arr4);
-console.log(arr6);
+// const arr6 = [...arr4];
+// // const arr6 = arr4;
+// console.log(arr6);
+// arr6[1] = 50;
+// console.log(arr4);
+// console.log(arr6);
 
-const arr7 = [...arr4, ...arr5];
-console.log(arr7);
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// 4-25　mapやfilterを使った配列の処理
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+nameArr.map((name, index) => console.log(`${index + 1}番目は${name}`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+const newNameArr = nameArr.map((name) => {
+  let outputName = name;
+  if (name !== "じゃけぇ") {
+    outputName += "さん";
+  }
+  return outputName;
+});
+console.log(newNameArr);
